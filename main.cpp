70e65c16f319d802461d52499a821c5b0c891ea7
amu <45954980+amubright@users.noreply.github.com>
2022-07-06 16:52:06 +0000

    Added program for finding pairs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "include/53_gi_find_pairs.h"
 
 int main() 
-{   
-  std::cout << "Hello World" << std::endl;
+{
+  std::vector<int> arr = {1, 2, 5, 3};
+  bool hasPair = hasPairWithSum (arr, 8);
+  std::cout << hasPair << std::endl;
 }
